71f39da5b5ea1f4c8af5dceceb02299d3f117693
CreativeTechnologist94 <asthakapila@gmail.com>
2022-06-25 20:33:34 -0700

    Completed Week 02 Assignment 04 - Mission Complete

--- Assets/Scripts/TriggerEnter.cs
+++ Assets/Scripts/TriggerEnter.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using StarterAssets;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class TriggerEnter : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    
+    [SerializeField] private GameObject _canvas;
+    [SerializeField] private GameObject _minimap;
+    [SerializeField] private GameObject particlefx;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.GameObject() == player)                   
         {
             Debug.Log("Player entered the goal.");
+            _canvas.SetActive(true);
+            particlefx.SetActive(true);
+            _minimap.SetActive(false);
+            player.gameObject.GetComponent<FirstPersonController>().enabled = false;
         }
     }
 }
