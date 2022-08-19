dacafe346bcd3787d93574850b4168f175e1497b
CreativeTechnologist94 <asthakapila@gmail.com>
2022-08-18 23:56:11 -0700

    Completed Week 07 Assignment 04 : Mobile Event Horizon

--- Assets/Scripts/Assignments/GravityManipulation/Body.cs
+++ Assets/Scripts/Assignments/GravityManipulation/Body.cs
@@ -6,17 +6,18 @@ public class Body : MonoBehaviour
 {
     [HideInInspector] public float mass;
     [HideInInspector] public Rigidbody rigidBody;
+    [HideInInspector] public MeshRenderer bodyRenderer;
+    public bool relocated;
+    public bool exploded;
     
     // Start is called before the first frame update
     void Start()
     {
         mass = GetComponent<Rigidbody>().mass;
         rigidBody = GetComponent<Rigidbody>();
+        bodyRenderer = GetComponent<MeshRenderer>();
+        relocated = false;
+        exploded = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Weapons/Bomb.cs
+++ Assets/Scripts/Weapons/Bomb.cs
@@ -1,78 +1,101 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
+using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class Bomb : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rb;
-    private bool _isGhost;
+    private MeshRenderer _renderer;
     private Body[] _bodies;
-    private bool _suckbodies = false;
-    private float _suction = 50f;
-    private float _blackholetime = 3f;
-    private float timer;
     private Vector3 _hitPos;
-    //variable to refernce renderer
+    
+    private bool _isGhost;
+    private float _multiplier = 100f; //0.5f
+    private int _counter;
 
     private void Start()
     {
+        _renderer = GetComponent<MeshRenderer>();
         _bodies = FindObjectsOfType<Body>();
-        
+        _counter = 0;
     }
     
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        if (!_suckbodies) return;
-        
+        if (!_isGhost) return;                                                                                                 //check if bomb landed
+
         foreach (var item in _bodies)
         {
-            if (timer < _blackholetime)
+            if (!item.relocated)                                                                                                //if it has not already relocated
             {
-                Vector3 dir = (item.transform.position - _hitPos).normalized;
-                //item.rigidBody.AddForce(dir * _suction, ForceMode.Force);
-                item.transform.position =
-                    Vector3.MoveTowards(item.transform.position, _hitPos, _suction * Time.deltaTime);
+                if (Vector3.Distance(_hitPos, item.transform.position) > 0.35f)                                                  //if distance is more from bomb
+                {
+                    item.rigidBody.velocity = (_hitPos - item.transform.position) * (_multiplier * Time.fixedDeltaTime);                   //add velocity
+                }
+                else                                                                                                                 //if distance is lesser
+                {
+                    item. rigidBody.velocity = Vector3.zero;                                                                               //stop velocity , relocate , add to counter
+                    item.bodyRenderer.enabled = false;
+                    _counter++;
+                    MoveBodyRandom(item);
+                }
             }
-            else if (timer == _blackholetime)
+            else                                                                                                                  //if it has relocated
             {
-                item.rigidBody.isKinematic = true;
-                item.transform.GetComponent<MeshRenderer>().enabled = false;
-                item.transform.position = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1)) + Vector3.up; ///world space
-                
+                if (_counter >=_bodies.Length && !item.exploded)                                                                       //if counter has reached length & item has not exploded                                        
+                {
+                    AddExplosionforce(item);                                                                                                 //explode
+                }
             }
-            else if (timer == (_blackholetime + 2))
-            {
-                item.transform.GetComponent<MeshRenderer>().enabled = true;
-                item.rigidBody.isKinematic = false;
-                _suckbodies = false;
-            }
-            
         }
-        
-        timer += Time.deltaTime;
     }
+
+    private void AddExplosionforce(Body body)
+    {
+        body.bodyRenderer.enabled = true;
+        body.rigidBody.AddExplosionForce(20f, _hitPos, 6f, 3f);
+        body.exploded = true;
+    }
+
+    private void MoveBodyRandom(Body body)
+    {
+        float x = Random.Range(-2f, 2f);
+        float y = Random.Range(0.5f, 2.5f);
+        float z = Random.Range(-2f, 2f);
+        body.transform.position += new Vector3(x, y, z);
+        body.relocated = true;
+    }
+
     public void Init(Vector3 velocity, bool isGhost)
    {
        _isGhost = isGhost;
        _rb.AddForce(velocity, ForceMode.Impulse);
    }
+   
     private void OnCollisionEnter(Collision other)
     {
         if (_isGhost) return;
         
         if (other.gameObject.CompareTag("Floor"))
         {
-            Debug.Log("first"); 
-            Destroy(gameObject);  
-            Debug.Log("second");
+            Debug.Log("hit floor");
             ContactPoint contact = other.contacts[0];
             _hitPos = contact.point;
+            _renderer.enabled = false;
+            _isGhost = true;
             
-            timer = 0f;
-            _suckbodies = true;
         }
     }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(_hitPos, new Vector3(0.2f, 0.2f, 0.2f));
+    }
+    
 }
